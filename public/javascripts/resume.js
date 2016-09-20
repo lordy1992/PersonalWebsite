c31f54f61b06b56b7efa04619d07f0f35ad9ebd9
Jeremy Lord <jlord_2006@hotmail.com>
2016-09-20 00:17:18 -0700

    Updating resume to have a cancel button

--- public/javascripts/resume.js
+++ public/javascripts/resume.js
@@ -34,21 +34,26 @@ function handleRemovePastExperience(elem) {
     $(elem).parent().remove();
 }
 
-function addDynamicEditor(listItem) {
+function addDynamicEditor(listItem, optText) {
     listItem.append('<textarea id="experience-description-' + listItem.index() + '" rows="10" cols="100"></textarea>')
     var editElem = listItem.find('textarea')[0];
+
+    if (optText !== undefined) {
+        listItem.find('textarea').text(optText);
+    }
+
     return new nicEditor().panelInstance(editElem);
 }
 
 $(document).ready(function() {
     var editLinks = [
-        {edit: '#top-level-info-edit-link', form: '#top-level-info-form', successCallback: topLevelInfoCallback},
-        {edit: '#summary-edit-link', form: '#summary-info-form', successCallback: summaryCallback},
-        {edit: '#research-interests-edit-link', form: '#research-interests-info-form', successCallback: researchInterestCallback},
-        {edit: '#past-experience-edit-link', form: '#past-experience-info-form', dataCallback: formPastExperienceRequest},
-        {edit: '#expertise-edit-link', form: '#expertise-info-form'},
-        {edit: '#language-tech-edit-link', form: '#language-tech-info-form'},
-        {edit: '#education-edit-link', form: '#education-info-form'}];
+        {edit: '#top-level-info-edit-link', cancel: '#top-level-info-submit-cancel', form: '#top-level-info-form', successCallback: topLevelInfoCallback},
+        {edit: '#summary-edit-link', cancel: '#summary-submit-cancel', form: '#summary-info-form', successCallback: summaryCallback},
+        {edit: '#research-interests-edit-link', cancel: '#research-info-submit-cancel', form: '#research-interests-info-form', successCallback: researchInterestCallback},
+        {edit: '#past-experience-edit-link', cancel: '#past-experience-submit-cancel', form: '#past-experience-info-form', dataCallback: formPastExperienceRequest},
+        {edit: '#expertise-edit-link', cancel: '#expertise-submit-cancel', form: '#expertise-info-form'},
+        {edit: '#language-tech-edit-link', cancel: '#language-tech-submit-cancel', form: '#language-tech-info-form'},
+        {edit: '#education-edit-link', cancel: '#education-submit-cancel', form: '#education-info-form'}];
 
     var editors = [];
 
@@ -62,7 +67,9 @@ $(document).ready(function() {
     });
 
     $('#past-experience-list').children().each(function() {
-        editors[editors.length] = addDynamicEditor($(this));
+        var editorInd = editors.length;
+        var description = $('#experience-readable-text').find('.list-group-item').eq($(this).index()).find('.past-experience-description').html();
+        editors[editorInd] = addDynamicEditor($(this), description);
     });
 
     for (let i = 0; i < editLinks.length; i++) {
@@ -75,6 +82,14 @@ $(document).ready(function() {
             $(link.edit).hide();
             $(link.form).find("[type='submit']").parent().show();
         });
+
+        $(editLinks[i].cancel).click(function() {
+            var link = editLinks[i];
+            $(link.form).find('.locked-mode').show();
+            $(link.form).find('.editable-mode').hide();
+            $(link.edit).show();
+            $(link.form).find("[type='submit']").parent().hide();
+        })
 
         $(editLinks[i].form).submit(function(e) {
             e.preventDefault();
