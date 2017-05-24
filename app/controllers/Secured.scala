8c45b6a9cedb0c5309b2f42ec3a465d61e3c66ed
Jeremy Lord <jlord_2006@hotmail.com>
2017-05-23 23:16:46 -0700

    Adding server-side timeout for the admin account

--- app/controllers/Secured.scala
+++ app/controllers/Secured.scala
@@ -1,22 +1,45 @@
 package controllers
 
+import java.util.Calendar
+
+import play.api.Logger
 import play.api.mvc.{Result, _}
 
-/**
-  * Created by jlord on 5/8/2016.
-  */
 trait Secured {
   def username(request: RequestHeader) = request.session.get(Security.username)
 
-  def onUnauthorized(request: RequestHeader) = Results.Redirect(routes.AuthController.login)
+  def onUnauthorized(request: RequestHeader): Result = Results.Redirect(routes.AuthController.login)
 
   def withAuth(f: => String => Request[AnyContent] => Result) : EssentialAction = {
     withAuth(BodyParsers.parse.anyContent)(f)
   }
 
+  /*
+   * This is wrapped around actions that should only be allowed for authenticated users. In addition, it handles
+   * a server-side session timeout. Whenever this is called (when updates are made), the timeout is reset. If it
+   * times out, the user is redirected to the login page and the session is cleared.
+   */
   def withAuth[A](p: BodyParser[A])(f: => String => Request[A] => Result) = {
     Security.Authenticated(username, onUnauthorized) { user =>
-      Action(p)(request => f(user)(request))
+      Action(p)(request => {
+        val previousTime = request.session.get("userTime") getOrElse ""
+        val currentTime = Calendar.getInstance.getTimeInMillis
+        if (!previousTime.isEmpty) {
+          val previousTimeLong = previousTime.toLong
+          val timeoutInMilliseconds = 900000
+
+          Logger.info("Current time: " + currentTime + ", and prev time: " +
+            previousTimeLong + ", Difference: " + (currentTime - previousTimeLong))
+          if ((currentTime - previousTimeLong) > timeoutInMilliseconds) {
+            // The session has expired
+            onUnauthorized(request).withNewSession.flashing("success" -> "You have been logged out")
+          } else {
+            f(user)(request).withSession(request.session + ("userTime" -> currentTime.toString))
+          }
+        } else {
+          f(user)(request).withSession(request.session + ("userTime" -> currentTime.toString))
+        }
+      })
     }
   }
 }
